484412959b64feab094a617ed309fef7822a8b3c
Siddhesh-Mali <129844841+Siddhesh-Mali@users.noreply.github.com>
2024-03-18 16:44:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 function stringChop(str, size) {
-  // your code here
-}
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+      // Check if the remaining string length is less than the desired chunk size
+      const remainingLength = str.length - i;
+      const chunkSize = Math.min(size, remainingLength); // Use Math.min to get the appropriate size
+  
+      arr.push(str.substring(i, i + chunkSize));
+      i=i+size-1;
+    }
+    // console.log(arr);
+    return `[${arr.toString()}]`;
+	
+  }
 
 // Do not change the code below
 const str = prompt("Enter String.");
